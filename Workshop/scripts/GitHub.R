41f229a7c10eafae477a3e6b7b68336419124dd4
Workshop User <noreply@workshop.com>
2022-12-03 11:02:00 +0000

    separate github token script to separate file

---
+++ Workshop/scripts/GitHub.R
@@ -0,0 +1,18 @@
+
+#     ____ _ _   _   _       _       _____     _              
+#    / ___(_) |_| | | |_   _| |__   |_   _|__ | | _____ _ __  
+#   | |  _| | __| |_| | | | | '_ \    | |/ _ \| |/ / _ \ '_ \ 
+#   | |_| | | |_|  _  | |_| | |_) |   | | (_) |   <  __/ | | |
+#    \____|_|\__|_| |_|\__,_|_.__/    |_|\___/|_|\_\___|_| |_|
+#                                                             
+
+# Magic required to be able to save our changes to GitHub
+
+# Check which remote we are connected to
+usethis::git_remotes()
+
+# Create a new Personal Access Token (PAT)
+usethis::create_github_token(scopes = 'repo', description = 'R Workshop')
+
+# Save our Personal Access Token (PAT)
+gitcreds::gitcreds_set(url = "https://github.com")
--- Workshop/scripts/ep00-demo.R
+++ Workshop/scripts/ep00-demo.R
@@ -21,13 +21,3 @@ ggplot(diamonds) +
   geom_point(aes(x=carat, y=price, color=cut)) + 
   geom_smooth(aes(x=carat, y=price))
 
-
-
-
-
-# Magic required to be able to save our changes to GitHub
-
-usethis::git_remotes()
-usethis::create_github_token(scopes = 'repo', description = 'R Workshop')
-
-gitcreds::gitcreds_set(url = "https://github.com")
