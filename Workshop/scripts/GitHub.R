cfc0585cd19fed904db375faa0fc47cf6e00229b
Workshop User <noreply@workshop.com>
2022-12-04 23:21:01 +0000

    add "steps" to comments

--- Workshop/scripts/GitHub.R
+++ Workshop/scripts/GitHub.R
@@ -7,11 +7,17 @@
 
 # Magic required to be able to save our changes to GitHub using tokens
 
+# Step 0
+#
 # Check which remote we are connected to
 usethis::git_remotes()
 
+# Step 1
+#
 # Create a new Personal Access Token (PAT)
 usethis::create_github_token(scopes = 'repo', description = 'R Workshop')
 
+# Step 2
+#
 # Save our Personal Access Token (PAT)
 gitcreds::gitcreds_set(url = "https://github.com")
